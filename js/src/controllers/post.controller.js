5d2cea21fcb967d2de27c114825b9172b9ca17a8
ruviRedner <r.y.r7966@gmail.com>
2024-09-24 17:47:02 +0300

    add post controller

--- js/src/controllers/post.controller.js
+++ js/src/controllers/post.controller.js
@@ -1,3 +1,83 @@
 import express from 'express';
 const router = express.Router();
+router.get("/", async (req, res) => {
+    try {
+        res.status(200).json({
+            err: false,
+            message: "surry",
+            data: undefined
+        });
+    }
+    catch (err) {
+        res.status(400).json({
+            err: true,
+            message: "surry",
+            data: null
+        });
+    }
+});
+router.get("/search", async (req, res) => {
+    try {
+        res.status(200).json({
+            err: false,
+            message: "surry",
+            data: undefined
+        });
+    }
+    catch (err) {
+        res.status(400).json({
+            err: true,
+            message: "surry",
+            data: null
+        });
+    }
+});
+router.post("/", async (req, res) => {
+    try {
+        res.status(200).json({
+            err: false,
+            message: "surry",
+            data: undefined
+        });
+    }
+    catch (err) {
+        res.status(400).json({
+            err: true,
+            message: "surry",
+            data: null
+        });
+    }
+});
+router.get("/:id", async (req, res) => {
+    try {
+        res.status(200).json({
+            err: false,
+            message: "surry",
+            data: undefined
+        });
+    }
+    catch (err) {
+        res.status(400).json({
+            err: true,
+            message: "surry",
+            data: null
+        });
+    }
+});
+router.patch("/like/:id", async (req, res) => {
+    try {
+        res.status(200).json({
+            err: false,
+            message: "surry",
+            data: undefined
+        });
+    }
+    catch (err) {
+        res.status(400).json({
+            err: true,
+            message: "surry",
+            data: null
+        });
+    }
+});
 export default router;
--- src/controllers/post.controller.ts
+++ src/controllers/post.controller.ts
@@ -1,6 +1,114 @@
-import express,{Router} from 'express';
+import express,{Router,Request,Response} from 'express';
 
 const router:Router = express.Router();
+router.get("/",async(
+    req:Request
+    ,res:Response
+):Promise<void>=>{
+    try {
 
+        res.status(200).json({
+            err:false,
+            message:"surry",
+            data:undefined
+        })
+        
+    } catch (err) {
+        res.status(400).json({
+            err:true,
+            message:"surry",
+            data:null
+        })
+    }
+
+})
+
+router.get("/search",async(
+    req:Request
+    ,res:Response
+):Promise<void>=>{
+    try {
+
+        res.status(200).json({
+            err:false,
+            message:"surry",
+            data:undefined
+        })
+        
+    } catch (err) {
+        res.status(400).json({
+            err:true,
+            message:"surry",
+            data:null
+        })
+    }
+
+})
+
+router.post("/",async(
+    req:Request
+    ,res:Response
+):Promise<void>=>{
+    try {
+
+        res.status(200).json({
+            err:false,
+            message:"surry",
+            data:undefined
+        })
+        
+    } catch (err) {
+        res.status(400).json({
+            err:true,
+            message:"surry",
+            data:null
+        })
+    }
+
+})
+
+router.get("/:id",async(
+    req:Request
+    ,res:Response
+):Promise<void>=>{
+    try {
+
+        res.status(200).json({
+            err:false,
+            message:"surry",
+            data:undefined
+        })
+        
+    } catch (err) {
+        res.status(400).json({
+            err:true,
+            message:"surry",
+            data:null
+        })
+    }
+
+})
+
+router.patch("/like/:id",async(
+    req:Request
+    ,res:Response
+):Promise<void>=>{
+    try {
+
+        res.status(200).json({
+            err:false,
+            message:"surry",
+            data:undefined
+        })
+        
+    } catch (err) {
+        res.status(400).json({
+            err:true,
+            message:"surry",
+            data:null
+        })
+    }
+
+})
 
 export default router;
