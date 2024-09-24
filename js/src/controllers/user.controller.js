481095aacfee594916c190d5a9573f20f1a6f29e
ruviRedner <r.y.r7966@gmail.com>
2024-09-24 17:53:59 +0300

    add user controller template

--- js/src/controllers/user.controller.js
+++ js/src/controllers/user.controller.js
@@ -1,3 +1,102 @@
 import express from 'express';
 const router = express.Router();
+router.post("/register", async (req, res) => {
+    try {
+        res.status(200).json({
+            err: false,
+            message: "surry",
+            data: undefined
+        });
+    }
+    catch (err) {
+        res.status(400).json({
+            err: true,
+            message: "surry",
+            data: null
+        });
+    }
+});
+router.post("/follow", async (req, res) => {
+    try {
+        res.status(200).json({
+            err: false,
+            message: "surry",
+            data: undefined
+        });
+    }
+    catch (err) {
+        res.status(400).json({
+            err: true,
+            message: "surry",
+            data: null
+        });
+    }
+});
+router.get("/search", async (req, res) => {
+    try {
+        res.status(200).json({
+            err: false,
+            message: "surry",
+            data: undefined
+        });
+    }
+    catch (err) {
+        res.status(400).json({
+            err: true,
+            message: "surry",
+            data: null
+        });
+    }
+});
+//? type = mine || else
+router.get("/profile", async (req, res) => {
+    try {
+        res.status(200).json({
+            err: false,
+            message: "surry",
+            data: undefined
+        });
+    }
+    catch (err) {
+        res.status(400).json({
+            err: true,
+            message: "surry",
+            data: null
+        });
+    }
+});
+//? type = mine || else
+router.get("/followers", async (req, res) => {
+    try {
+        res.status(200).json({
+            err: false,
+            message: "surry",
+            data: undefined
+        });
+    }
+    catch (err) {
+        res.status(400).json({
+            err: true,
+            message: "surry",
+            data: null
+        });
+    }
+});
+//? type = mine || else
+router.get("/following", async (req, res) => {
+    try {
+        res.status(200).json({
+            err: false,
+            message: "surry",
+            data: undefined
+        });
+    }
+    catch (err) {
+        res.status(400).json({
+            err: true,
+            message: "surry",
+            data: null
+        });
+    }
+});
 export default router;
--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,6 +1,139 @@
-import express,{Router} from 'express';
+import express,{Request, Router,Response} from 'express';
 
 const router:Router = express.Router();
+router.post("/register",async(
+    req:Request
+    ,res:Response
+):Promise<void>=>{
+    try {
+
+        res.status(200).json({
+            err:false,
+            message:"surry",
+            data:undefined
+        })
+        
+    } catch (err) {
+        res.status(400).json({
+            err:true,
+            message:"surry",
+            data:null
+        })
+    }
+
+})
+
+router.post("/follow",async(
+    req:Request
+    ,res:Response
+):Promise<void>=>{
+    try {
+
+        res.status(200).json({
+            err:false,
+            message:"surry",
+            data:undefined
+        })
+        
+    } catch (err) {
+        res.status(400).json({
+            err:true,
+            message:"surry",
+            data:null
+        })
+    }
+
+})
+
+router.get("/search",async(
+    req:Request
+    ,res:Response
+):Promise<void>=>{
+    try {
+
+        res.status(200).json({
+            err:false,
+            message:"surry",
+            data:undefined
+        })
+        
+    } catch (err) {
+        res.status(400).json({
+            err:true,
+            message:"surry",
+            data:null
+        })
+    }
+
+})
+
+//? type = mine || else
+router.get("/profile",async(
+    req:Request
+    ,res:Response
+):Promise<void>=>{
+    try {
+
+        res.status(200).json({
+            err:false,
+            message:"surry",
+            data:undefined
+        })
+        
+    } catch (err) {
+        res.status(400).json({
+            err:true,
+            message:"surry",
+            data:null
+        })
+    }
+
+})
+
+//? type = mine || else
+router.get("/followers",async(
+    req:Request
+    ,res:Response
+):Promise<void>=>{
+    try {
+
+        res.status(200).json({
+            err:false,
+            message:"surry",
+            data:undefined
+        })
+        
+    } catch (err) {
+        res.status(400).json({
+            err:true,
+            message:"surry",
+            data:null
+        })
+    }
+
+})
 
 
+//? type = mine || else
+router.get("/following",async(
+    req:Request
+    ,res:Response
+):Promise<void>=>{
+    try {
+
+        res.status(200).json({
+            err:false,
+            message:"surry",
+            data:undefined
+        })
+        
+    } catch (err) {
+        res.status(400).json({
+            err:true,
+            message:"surry",
+            data:null
+        })
+    }
+
+})
 export default router;
